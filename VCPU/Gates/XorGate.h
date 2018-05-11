85c14171dff78b6c807b871b30ede57b2225bdf2
David Payne <dwrpayne@gmail.com>
2018-05-11 01:09:44 -0700

    XorGateN

--- VCPU/Gates/XorGate.h
+++ VCPU/Gates/XorGate.h
@@ -19,3 +19,46 @@ private:
 	OrGate or;
 	AndGate and;
 };
+
+
+template <unsigned int N>
+class XorGateN : public Component
+{
+public:
+	XorGateN();
+	void Connect(const Bundle<N>& a, const Bundle<N>& b);
+	void Update();
+
+	const Bundle<N>& Out() { return out; }
+
+private:
+	XorGate xors[N];
+	Bundle<N> out;
+};
+
+template<unsigned int N>
+inline XorGateN<N>::XorGateN()
+{
+	for (int i = 0; i < N; ++i)
+	{
+		out.Connect(i, xors[i].Out());
+	}
+}
+
+template<unsigned int N>
+inline void XorGateN<N>::Connect(const Bundle<N>& a, const Bundle<N>& b)
+{
+	for (int i = 0; i < N; ++i)
+	{
+		xors[i].Connect(a[i], b[i]);
+	}
+}
+
+template<unsigned int N>
+inline void XorGateN<N>::Update()
+{
+	for (auto& xor : xors)
+	{
+		xor.Update();
+	}
+}
