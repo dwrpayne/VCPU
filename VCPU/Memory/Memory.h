e2a0d6bae97a61035f08acea3d5cf10248e0ba11
David Payne <dwrpayne@gmail.com>
2018-05-13 12:29:19 -0700

    Single addr memory bank

---
+++ VCPU/Memory/Memory.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include "Component.h"
+#include "Bundle.h"
+#include "Register.h"
+#include "Decoder.h"
+#include "MultiGate.h"
+#include "MuxBundle.h"
+
+
+template <unsigned int N, unsigned int NReg>
+class Memory : public Component
+{
+public:
+	static const unsigned int ADDR_BITS = bits(NReg);
+	typedef Bundle<ADDR_BITS> AddrBundle;
+	typedef Bundle<N> DataBundle;
+
+	void Connect(const AddrBundle& addr, const DataBundle& data, const Wire& write);
+	void Update();
+
+	const DataBundle& Out() { return outMux.Out(); }
+
+private:
+	Decoder<NReg> addrDecoder;
+	MultiGate<AndGate, NReg> writeEnable;
+	std::array<Register<N>, NReg> registers;
+	MuxBundle<N, NReg> outMux;
+};
+
+template<unsigned int N, unsigned int NReg>
+inline void Memory<N, NReg>::Connect(const AddrBundle & addr, const DataBundle & data, const Wire& write)
+{
+	addrDecoder.Connect(addr);
+	writeEnable.Connect(addrDecoder.Out(), Bundle<NReg>(write));
+
+	std::array<DataBundle, NReg> regOuts;
+	for (int i = 0; i < NReg; ++i)
+	{
+		registers[i].Connect(data, writeEnable.Out()[i]);
+		regOuts[i] = registers[i].Out();
+	}
+	outMux.Connect(regOuts, addr);
+}
+
+template<unsigned int N, unsigned int NReg>
+inline void Memory<N, NReg>::Update()
+{
+	addrDecoder.Update();
+	writeEnable.Update();
+	for (auto& reg : registers)
+	{
+		reg.Update();
+	}
+	outMux.Update();
+}
