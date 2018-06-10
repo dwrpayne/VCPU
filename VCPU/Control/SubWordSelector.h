0055c21db730d66a2ae60c47507312a8afb47cb6
David Payne <dwrpayne@gmail.com>
2018-06-09 20:36:00 -0700

    Move SubWordSelector into /Control

--- VCPU/Control/SubWordSelector.h
+++ VCPU/Control/SubWordSelector.h
@@ -3,6 +3,7 @@
 #include "Component.h"
 #include "MuxBundle.h"
 #include "Bundle.h"
+#include "Extender.h"
 
 
 // Given N-bit word and a selector Bundle, mask out the selected partial word and output it in the least-sig part of out
@@ -13,24 +14,15 @@ class SubWordSelector : public Component
 public:
 	static const int PARTS = N / Nsubsize;
 	static const int ADDR = bits(PARTS);
-	SubWordSelector();
 	void Connect(const Bundle<N>& in, const Bundle<ADDR>& sel, const Wire& signextend);
 	void Update();
 
-	const Bundle<N>& Out() const { return out; }
+	const Bundle<N>& Out() const { return extender.Out(); }
 
 private:
 	MuxBundle<Nsubsize, PARTS> byteMux;
-	Multiplexer<2> fillMux;
-	Bundle<N> out;
+	Extender<Nsubsize, N> extender;
 };
-
-template <unsigned int N, unsigned int Nsubsize>
-inline SubWordSelector<N, Nsubsize>::SubWordSelector()
-	: out(fillMux.Out())
-{
-	out.Connect(0, byteMux.Out());
-}
 
 template <unsigned int N, unsigned int Nsubsize>
 inline void SubWordSelector<N, Nsubsize>::Connect(const Bundle<N>& in, const Bundle<ADDR>& sel, const Wire & signextend)
@@ -41,12 +33,12 @@ inline void SubWordSelector<N, Nsubsize>::Connect(const Bundle<N>& in, const Bun
 		bytes[i] = in.Range<Nsubsize>(i * Nsubsize);
 	}
 	byteMux.Connect(bytes, sel);
-	fillMux.Connect({ &byteMux.Out()[Nsubsize-1], &Wire::OFF }, signextend);
+	extender.Connect(byteMux.Out(), signextend);
 }
 
 template <unsigned int N, unsigned int Nsubsize>
 inline void SubWordSelector<N, Nsubsize>::Update()
 {
 	byteMux.Update();
-	fillMux.Update();
+	extender.Update();
 }
