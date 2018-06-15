5895b7b4a649b21a96adbb4649ae1bffb2b8b6a0
David Payne <dwrpayne@gmail.com>
2018-06-15 14:46:18 -0700

    Implement Baugh-Wooley signed integer Multiplier.

--- VCPU/Functional/Multiplier.h
+++ VCPU/Functional/Multiplier.h
@@ -3,66 +3,111 @@
 #include "Component.h"
 #include "Wire.h"
 #include "Bundle.h"
-#include "Inverter.h"
 #include "FullAdder.h"
-#include "Multiplexer.h"
-#include "MuxBundle.h"
-#include "OverflowDetector.h"
+#include "NandGate.h"
 
-template <unsigned int N>
-class Multiplier : public Component
+// Implementation of a Baugh-Wooley signed integer multiplier: http://www.ece.uvic.ca/~fayez/courses/ceng465/lab_465/project2/multiplier.pdf
+
+template<typename GateType>
+class BaughWooleyCell : public Component
 {
 public:
-	Multiplier();
+	void Connect(const Wire& a, const Wire& b, const Wire& c, const Wire& s)
+	{
+		gate.Connect(a, b);
+		adder.Connect(a, s, c);
+	}
+	void Update()
+	{
+		gate.Update();
+		adder.Update();
+	}
+	const Wire& S() { return adder.S(); }
+	const Wire& C() { return adder.Cout(); }
+private:
+	GateType gate;
+	FullAdder adder;
+};
+
+typedef BaughWooleyCell<AndGate> BaughWooleyWhite;
+typedef BaughWooleyCell<NandGate> BaughWooleyGrey;
+
+template <unsigned int N>
+class BaughWooleyMultiplier : public Component
+{
+public:
+	BaughWooleyMultiplier();
 	void Connect(const Bundle<N>& a, const Bundle<N>& b);
 	void Update();
 
-	const Bundle<N*2>& Out() const { return out; }
+	const Bundle<N * 2>& Out() const { return out; }
 
 private:
-	std::array<MultiGate<AndGate, N>, N> ands;
-	std::array<CarrySaveAdderN<N>, N-1> adders;
+	std::array<std::array<Component*, N>, N> cells;
+
 	FullAdderN<N> outAdder;
 	Bundle<N * 2> out;
 };
 
 template<unsigned int N>
-inline Multiplier<N>::Multiplier()
+inline BaughWooleyMultiplier<N>::BaughWooleyMultiplier()
 {
-	out.Connect(0, ands[0].Out()[0]);
-	for (int i = 0; i < N-1; ++i)
+	for (int i = 0; i < N; i++)
 	{
-		out.Connect(i+1, adders[i].Out()[0]);
+		for (int j = 0; j < N; j++)
+		{
+			if ((i == N - 1) ^ (j == N - 1))
+			{
+				cells[i][j] = new BaughWooleyGrey();
+			}
+			else
+			{
+				cells[i][j] = new BaughWooleyWhite();
+			}
+		}
+	}
+
+	for (int i = 0; i < N; ++i)
+	{
+		out.Connect(i, cells[0][i].S());
 	}
 	out.Connect(N, outAdder.Out());
 }
 
 template<unsigned int N>
-inline void Multiplier<N>::Connect(const Bundle<N>& a, const Bundle<N>& b)
+inline void BaughWooleyMultiplier<N>::Connect(const Bundle<N>& a, const Bundle<N>& b)
 {
-	for (int i = 0; i < N; i++)
+	Bundle<N> sums, carries;
+	for (int i = 0; i < N - 1; i++)
 	{
-		ands[i].Connect(Bundle<N>(a[i]), b);
+		cells[i][0]->Connect(a[i], b[0], Wire::OFF, Wire::OFF);
+		for (int j = 1; j < N; j++)
+		{
+			cells[i][j]->Connect(a[i], b[j], cells[i][j - 1]->C(), cells[i + 1][j - 1]->S());
+		}
+		sum.Connect(i, cells[i + 1][N - 1]->S());
+		carries.Connect(i, cells[i][N - 1]->C());
 	}
-	adders[0].Connect(ands[1].Out(), ands[0].Out().ShiftRightWireExtend<N>(1, Wire::OFF), Bundle<N>::OFF);
-	for (int i = 1; i < N-1; i++)
+	for (int j = 1; j < N; j++)
 	{
-		adders[i].Connect(ands[i + 1].Out(), adders[i - 1].Out().ShiftRightWireExtend<N>(1, Wire::OFF),
-			adders[i - 1].Cout());
+		cells[N - 1][j]->Connect(a[N - 1], b[j], cells[i][j - 1]->C(), Wire::OFF);
 	}
-	outAdder.Connect(adders[N - 2].Out().ShiftRightWireExtend<N>(1, Wire::OFF), adders[N - 2].Cout(), Wire::OFF);
+	sum.Connect(N - 1, Wire::ON);
+	carries.Connect(N - 1, cells[N - 1][N - 1]->C());
+
+	outAdder.Connect(sums, carries, Wire::ON);
 }
 
 template<unsigned int N>
-inline void Multiplier<N>::Update()
+inline void BaughWooleyMultiplier<N>::Update()
 {
 	for (int i = 0; i < N; i++)
 	{
-		ands[i].Update();
-	}
-	for (int i = 0; i < N-1; i++)
-	{
-		adders[i].Update();
+		for (int j = 0; j < N; j++)
+		{
+			cells[j][i]->Update();
+		}
 	}
 	outAdder.Update();
 }
+
