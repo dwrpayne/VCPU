2bc31e76d3b0474b492ed27628afd1360b725f5b
David Payne <dwrpayne@gmail.com>
2018-06-20 13:21:45 -0700

    CircularBuffer fixes
    Handle some new cases: 
    
    Push and Pop on an empty queue -> forward the push data directly to output.
    Push and Pop on a full queue -> return the data at the read target before overwriting it.
    
    Hold the popped data on an output register until new data is popped.

--- VCPU/Memory/CircularBuffer.h
+++ VCPU/Memory/CircularBuffer.h
@@ -15,10 +15,10 @@ class CircularBuffer : Component
 public:
 	static const int ADDR_LEN = bits(Nreg);
 	typedef Bundle<ADDR_LEN> AddrBundle;
-	
+
 	void Connect(const Bundle<N>& in, const Wire & pop, const Wire& push);
 	void Update();
-	const Bundle<N>& Out() { return outMux.Out(); }
+	const Bundle<N>& Out() { return popBuffer.Out(); }
 
 	const Wire& Full() { return counters.Full(); }
 	const Wire& NonEmpty() { return counters.NonEmpty(); }
@@ -52,21 +52,26 @@ private:
 	};
 
 	CounterPair counters;
+	OrGate couldPush;
 	AndGate pushEnable;
 	OrGate couldPop;
 	AndGate popEnable;
 	Decoder<Nreg> writeDecoder;
 	std::array<Register<N>, Nreg> buffers;
 	MuxBundle<N, Nreg> outMux;
+	MuxBundle<N, 2> forwardPushMux;
+	Register<N> popBuffer;
 };
 
 template<unsigned int N, unsigned int Nreg>
 inline void CircularBuffer<N, Nreg>::Connect(const Bundle<N>& in, const Wire & pop, const Wire& push)
 {
-	pushEnable.Connect(push, counters.HasRoom());
+	couldPush.Connect(pop, counters.HasRoom());
+	pushEnable.Connect(push, couldPush.Out());
 	couldPop.Connect(pushEnable.Out(), counters.NonEmpty());
 	popEnable.Connect(pop, couldPop.Out());
 	counters.Connect(popEnable.Out(), pushEnable.Out());
+
 	writeDecoder.Connect(counters.WriteIndex(), pushEnable.Out());
 	std::array<Bundle<N>, Nreg> regOuts;
 	for (int i = 0; i < Nreg; i++)
@@ -74,7 +79,10 @@ inline void CircularBuffer<N, Nreg>::Connect(const Bundle<N>& in, const Wire & p
 		buffers[i].Connect(in, writeDecoder.Out()[i]);
 		regOuts[i].Connect(0, buffers[i].Out());
 	}
+
 	outMux.Connect(regOuts, counters.ReadIndex());
+	forwardPushMux.Connect({ outMux.Out(), in }, counters.Empty());
+	popBuffer.Connect(forwardPushMux.Out(), popEnable.Out());
 
 	counters.Update();
 }
@@ -82,17 +90,21 @@ inline void CircularBuffer<N, Nreg>::Connect(const Bundle<N>& in, const Wire & p
 template<unsigned int N, unsigned int Nreg>
 inline void CircularBuffer<N, Nreg>::Update()
 {
+	couldPush.Update();
 	pushEnable.Update();
 	couldPop.Update();
 	popEnable.Update();
-	counters.Update();
 
 	writeDecoder.Update();
 	for (int i = 0; i < Nreg; i++)
 	{
 		buffers[i].Update();
 	}
+	forwardPushMux.Update();
+	popBuffer.Update();
+	counters.Update();
 	outMux.Update();
+
 }
 
 template<unsigned int N, unsigned int Nreg>
