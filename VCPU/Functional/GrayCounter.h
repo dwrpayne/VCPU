f578a1bead982ebbe5002019926846e48efea53a
David Payne <dwrpayne@gmail.com>
2018-06-21 18:13:07 -0700

    GrayCounter Component

---
+++ VCPU/Functional/GrayCounter.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "Component.h"
+#include "Bundle.h"
+#include "JKFlipFlop.h"
+#include "AndGate.h"
+#include "MuxBundle.h"
+#include "Inverter.h"
+
+template <unsigned int N>
+class GrayCounter : public Component
+{
+public:
+	void Connect(const Wire& clear, const Wire& enable);
+	void Update();
+
+	const Bundle<N>& OutBin() const { return counter.Out(); }
+	const Bundle<N>& OutGray() const { return gray.Out(); }
+
+private:
+	Counter<N> counter;
+	BinaryToGray<N> gray;
+};
+
+template<unsigned int N>
+inline void GrayCounter<N>::Connect(const Wire& clear, const Wire& enable)
+{
+	counter.Connect(clear, enable);
+	gray.Connect(counter.Out());
+}
+
+template<unsigned int N>
+inline void GrayCounter<N>::Update()
+{
+	counter.Update();
+	gray.Update();
+}
+
